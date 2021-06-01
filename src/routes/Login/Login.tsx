692e7af4206fc160b6f03856efa99fa44a18dc57
ghedzko <ghedzko@gmail.com>
2021-05-31 22:08:58 -0300

    feat: agregamos sass y estilos del form

--- src/routes/Login/Login.tsx
+++ src/routes/Login/Login.tsx
@@ -1,10 +1,22 @@
 import React from "react";
+import "./Login.scss";
 export interface LoginProps {}
 
 export const Login: React.FC<LoginProps> = () => {
   return (
-    <div className="Login">
-      <h1>Login Please</h1>
+    <div className="form-container">
+      <form className="form-container__form" action="#">
+        <div>
+          <label htmlFor="username">Nombre de Usuario</label>
+          <input type="text" id="username" />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="text" id="password" />
+        </div>
+
+        <button onClick={() => {}}>Ingresar</button>
+      </form>
     </div>
   );
 };
