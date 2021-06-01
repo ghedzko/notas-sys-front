cd4efe48dc30eec61f4bf4cef9c5cb7c22cdb337
ghedzko <ghedzko@gmail.com>
2021-05-31 23:09:06 -0300

    feat: add state to Input component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -5,12 +5,18 @@ export interface InputProps {
   label: string;
   id: string;
   autofocus?: boolean;
+  value: string;
+  onChange: (arg0: any) => void;
+  type?: string;
 }
 
 export const Input: React.FC<InputProps> = ({
   label,
   id,
   autofocus = false,
+  value,
+  onChange,
+  type = "text",
 }) => {
   return (
     <div className="input-container">
@@ -19,9 +25,11 @@ export const Input: React.FC<InputProps> = ({
       </label>
       <input
         className="input-container__input"
-        type="text"
+        type={type}
         id={id}
         autoFocus={autofocus}
+        value={value}
+        onChange={onChange}
       />
     </div>
   );
--- src/routes/Login/Login.tsx
+++ src/routes/Login/Login.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Input } from "../../components/Input";
 
 import "./Login.scss";
@@ -6,12 +6,35 @@ import "./Login.scss";
 export interface LoginProps {}
 
 export const Login: React.FC<LoginProps> = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handlerUserChange = (e: any) => {
+    setUserName(e.target.value);
+  };
+
+  const handlerPasswordChange = (e: any) => {
+    setPassword(e.target.value);
+  };
+
   return (
     <div className="form-container">
       <form className="form-container__form" action="#">
-        <Input label="Nombre de Usuario" id="username" autofocus />
+        <Input
+          label="Nombre de Usuario"
+          id="username"
+          autofocus
+          value={userName}
+          onChange={handlerUserChange}
+        />
 
-        <Input label="Password" id="password" />
+        <Input
+          label="Password"
+          id="password"
+          value={password}
+          onChange={handlerPasswordChange}
+          type="password"
+        />
 
         <button onClick={() => {}}>Ingresar</button>
       </form>
