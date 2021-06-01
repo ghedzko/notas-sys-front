e2803ab1335511c51493b795687139441648c040
ghedzko <ghedzko@gmail.com>
2021-05-31 21:26:52 -0300

    feat: add Login route

---
+++ src/routes/Login/Login.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+export interface LoginProps {}
+
+export const Login: React.FC<LoginProps> = () => {
+  return (
+    <div className="Login">
+      <h1>Login Please</h1>
+    </div>
+  );
+};
